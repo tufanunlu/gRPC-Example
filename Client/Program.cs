4c2d0d5b632e83fe8345250b1e0dcf7599c1c4fc
tunlu <tunlu@innova.com.tr>
2022-02-28 02:39:35 +0300

    initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,17 @@
+﻿
+while (true)
+{
+    Console.WriteLine("Enter Id");
+    int Id = Convert.ToInt32(Console.ReadLine());
+
+    using (var channel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:5178/"))
+    {
+        var client = new WebApiServer.DemoProtos.DemoService.DemoServiceClient(channel);
+        var request = new WebApiServer.DemoProtos.DemoRequest { Id = Id };
+        var result = client.GetDemoData(request);
+        Console.WriteLine(result.Id);
+        Console.WriteLine(result.Name);
+        Console.WriteLine("-----");
+    }
+}
+
---
+++ WebApiServer/Services/DemoService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using WebApiServer.DemoProtos;
+
+namespace WebApiServer.Services
+{
+    public class DemoService : WebApiServer.DemoProtos.DemoService.DemoServiceBase
+    {
+        public override Task<DemoResponse> GetDemoData(DemoRequest demoRequest, ServerCallContext context)
+        {
+            Dictionary<int, string> data = new Dictionary<int, string>() { { 1, "Adana" }, { 6, "Ankara" }, { 34, "İstanbul" }, { 41, "Kocaeli" }, { 58, "Sivas" }, { 59, "Tekirdağ" } };
+
+            string name;
+            data.TryGetValue(demoRequest.Id, out name);
+
+            return Task.FromResult(new DemoResponse
+            {
+                Id = demoRequest.Id,
+                Name = name
+            });
+        }
+    }
+}
